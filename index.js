fe19ddeb61cd36748128c06ee6c69d1bafbac18a
Timur <sol485@outlook.com>
2019-11-30 09:18:30 +0300

    Added a short readme

--- index.js
+++ index.js
@@ -1,46 +1,26 @@
 const Discord = require('discord.js');
+const { token, prefix } = require('./config.json');
 const client = new Discord.Client();
 
-const token = 'NjQ2Nzk0MDYwMTc0MTk2NzQ2.XdWU5A.iLb5gDdpqQ-8kqS4v_NsLhYBjHo';
-
-const PREFIX = '!';
-
-client.on('ready', () => {
-  console.log('This bot is online!');
+client.once('ready', () => {
+	console.log('This bot is online!');
 });
 
-client.on('message', (msg) => {
-  let args = msg.content.substring(PREFIX.length).split(' ');
+client.on('message', message => {
+	const args = message.content.substring(prefix.length).split(' ');
 
-  switch (args[0]) {
-    case 'ping':
-      msg.channel.send('pong!');
-      break;
-    case 'website':
-      msg.channel.send('youtube.com');
-      break;
-    case 'info':
-      if (args[1] == 'version') {
-        msg.channel.send('Version 1.0.0');
-      } else {
-        msg.channel.send('Invalid arguments');
-      }
-      break;
-    case 'clear':
-      if (!args[1]) return msg.reply('Error, please define second argument');
-      msg.channel.bulkDelete(args[1] + 1);
-      break;
-    case 'embed':
-      const embed = new Discord.RichEmbed()
-        .setAuthor('Dick Douglas')
-        .setColor('BLUE')
-        .setFooter('long dick style')
-        .addField('Author\'s Daddy', msg.author.username);
-      msg.channel.sendEmbed(embed);
-
-      break;
-  } 
-
+	if (message.content.startsWith(`${prefix}ping`)) {
+		message.channel.send('Pong.');
+	}
+	else if (message.content.startsWith(`${prefix}beep`)) {
+		message.channel.send('Boop.');
+	}
+	else if (message.content.startsWith(`${prefix}server`)) {
+		message.channel.send(`Server name: ${message.guild.name}\nTotal population: ${message.guild.memberCount}`);
+	}
+	else if (message.content === `${prefix}user-info`) {
+		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+	}
 });
 
 client.login(token);
